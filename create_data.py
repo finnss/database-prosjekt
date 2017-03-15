6f01218196a77f327935245d8779eee4aa21fa83
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2017-03-15 10:02:06 +0100

    Expanded create_data.py

--- create_data.py
+++ create_data.py
@@ -15,7 +15,16 @@ add_user = ("INSERT INTO bruker "
             "(brukernavn,kjønn,alder) "
             "VALUES (%s, %s, %s)")
 
+add_har_trent = ("INSERT INTO har_trent"
+                 "(brukernavn,øktid,dato,Tidspunkt,prestasjon,personlig_form)"
+                 "VALUES (%s,%s,%s,%s,%s,%s)")
+
+add_treningsøkt = ("INSERT INTO treningsøkt "
+            "(øktid,brukerid,tidspunkt,idrett)"
+            "VALUES (%s, %s, %s, %s)")
+
 simen = ("Simen Haga","mann","22")
+økt = ("1",simen[0],"2017-03-15","vektløfting")
 
 
 cursor.execute(add_user, simen)
