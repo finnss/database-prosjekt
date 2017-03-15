ca76bb9118c83a13d7ec6e481d4cd1bb2d468f84
Jens-Andreas Rensaa <jens.rensaa@gmail.com>
2017-03-15 12:09:16 +0100

    Extended test data generation

--- create_data.py
+++ create_data.py
@@ -11,17 +11,53 @@ config = {
 cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
 
-add_user = ("INSERT INTO bruker "
-            "(brukernavn,kjønn,alder) "
-            "VALUES (%s, %s, %s)")
+add_user = (
+    " INSERT INTO bruker "
+    " (brukernavn,kjønn,alder) "
+    " VALUES (%s, %s, %s)"
+)
 
-add_har_trent = ("INSERT INTO har_trent"
-                 "(brukernavn,øktid,dato,Tidspunkt,prestasjon,personlig_form)"
-                 "VALUES (%s,%s,%s,%s,%s,%s)")
+add_har_trent = (
+    "INSERT INTO har_trent"
+    "(brukernavn,øktid,dato,Tidspunkt,prestasjon,personlig_form)"
+    "VALUES (%s,%s,%s,%s,%s,%s)"
+)
 
-add_treningsøkt = ("INSERT INTO treningsøkt "
-            "(øktid,brukerid,tidspunkt,idrett)"
-            "VALUES (%s, %s, %s, %s)")
+add_treningsøkt = (
+    "INSERT INTO treningsøkt "
+    "(øktid,brukerid,tidspunkt,idrett)"
+    "VALUES (%s, %s, %s, %s)"
+)
+
+add_innendørsøkt = (
+    "INSERT INTO innendørsøkt "
+    "(øktid,ventilasjon,antall_tilskuere) "
+    "VALUES (%s, %s, %s)"
+)
+
+add_utendørsøkt = (
+    "INSERT INTO utendørsøkt "
+    "(øktid,Temperatur,værtype) "
+    "VALUES (%s, %s, %s)"
+)
+
+add_utført = (
+    "INSERT INTO utført"
+    "(repetisjoner,sett,vekt,lengde,kommentar,øktid,øvelse_navn)"
+    "VALUES (%s,%s,%s,%s,%s,%s,%s)"
+)
+
+add_øvelse = (
+    "INSERT INTO øvelse"
+    "(øvelse_navn,beskrivelse,antall_reps,antall_sett,belastning)"
+    "VALUES (%s,%s,%s,%s,%s)"
+)
+
+add_mål = (
+    "INSERT INTO utført"
+    "(målid,tidsfrist,repetisjoner,sett,vekt,lengde,kommentar,brukernavn,øvelse_navn)"
+    "VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+)
 
 simen = ("Simen Haga","mann","22")
 økt = ("1","1","2017-03-15","vektløfting")
